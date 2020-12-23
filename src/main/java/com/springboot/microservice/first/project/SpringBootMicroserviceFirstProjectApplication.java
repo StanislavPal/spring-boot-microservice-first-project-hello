0d27fb8d19d8e3338b27180d80b11b75ac435c64
StanislavPal <stasunado@gmail.com>
2020-12-23 19:28:27 +0400

    1

---
+++ src/main/java/com/springboot/microservice/first/project/SpringBootMicroserviceFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservice.first.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroserviceFirstProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/microservice/first/project/SpringBootMicroserviceFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservice.first.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceFirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
